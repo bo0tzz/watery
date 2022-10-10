70a4cc79e41981fe1cc4c5344870a75f6902f9c1
bo0tzz <git@bo0tzz.me>
2022-10-10 15:32:33 +0200

    Clean up days_since_watered logic

--- lib/watery_web/views/page_view.ex
+++ lib/watery_web/views/page_view.ex
@@ -6,23 +6,29 @@ defmodule WateryWeb.PageView do
     |> Timex.Format.Duration.Formatters.Humanized.format()
   end
 
-  def pretty_days_since(moment) do
-    Timex.Interval.new(from: moment, until: DateTime.utc_now())
-    |> Timex.Interval.duration(:days)
+  def pretty_days_since_watered(plant) do
+    days_since_watered(plant)
     |> case do
       0 -> "Today"
       days -> pretty_frequency(days) <> " ago"
     end
+    |> dbg()
   end
 
   def color(plant) do
-    IO.inspect(plant)
-
-    Timex.diff(DateTime.utc_now(), plant.last_watered, :days)
-    |> IO.inspect()
+    days_since_watered(plant)
     |> case do
       days when days > plant.frequency -> "color: red"
+      days when days == plant.frequency -> "color: orange"
       _ -> "color: green"
     end
+    |> dbg()
+  end
+
+  def days_since_watered(plant) do
+    plant.last_watered
+    |> DateTime.to_date()
+    |> Date.diff(Date.utc_today())
+    |> Kernel.*(-1)
   end
 end
